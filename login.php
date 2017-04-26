c6de3417cd11f6bc0693d6153386c290f47c0167
Khalodation <a.khaledshehab@gmail.com>
2017-04-26 03:58:15 +0200

    trying to display information

--- login.php
+++ login.php
@@ -10,10 +10,10 @@ require 'dbconfig/config.php';
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link rel="stylesheet" href="try.css"> 
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script> 
-<script src="javascript/jquery.js"></script> 
-<script src="javascript/jquery.validate.js"></script>   
+<link rel="stylesheet" href="try.css">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+<script src="javascript/jquery.js"></script>
+<script src="javascript/jquery.validate.js"></script>
 <script type="text/javascript" src="javascript/javascript.js"></script>
 <title>Login</title>
 </head>
@@ -23,52 +23,52 @@ require 'dbconfig/config.php';
     <div id="divheader"> <h1> Insurancy</h1></div>
           <div id="divLine"></div>
            <div id="divLinks">
-           <div id="divNav"> 
+           <div id="divNav">
                 <ul>
                     <li><a href="index.html" >Home</a></li>
                     <li><a href="services.html" >Services</a></li>
                     <li><a href="contact us.html">Contact us</a></li>
                     <li><a href="login.html"class="current">Log in</a></li>
-                
+
                 </ul>
 
             </div>
-           
-         
 
-           
+
+
+
            </div>
    <marquee> Put your Ads here</marquee>
 
 <section id="maincontent">
 <div id="container">
-<div id="tabbox">           
+<div id="tabbox">
 <a href="#" id="signin" class="tab select">Sign in</a>
 <a href="#" id="signup" class="tab signup">Sign up</a>
 </div>
 <div id="formpanel">
-    
+
     <!-- goz2 el sign in -->
 <div id="signinbox">
 <form action="login.php" id="signinform" method="post">
 <label for="username">Username</label>
 <input type="username" name="username" id="username" class="txtfield"  autocomplete="off">
 <label for="password">Password </label>
-<input type="password" name="password" id="password" class="txtfield"  autocomplete="off">           
+<input type="password" name="password" id="password" class="txtfield"  autocomplete="off">
 <input type="submit" name="signinbtn" id="submitsignin" value="Sign in" class="btn" >
 </form>
 <?php
 if (isset($_POST['signinbtn'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
-  
+
 
 $query = "select * from patient WHERE E_MAIL='$username' AND USER_PASSWORD='$password'";
 $query_run = mysqli_query($con,$query);
 
 if (mysqli_num_rows($query_run)>0) {
-  
-  
+
+$_SESSION['username']= $username;
   header('location:profile.php');
 
 }
@@ -86,43 +86,43 @@ else {
 
 
 </div>
-    
-      <!-- goz2 el sign up --> 
-    
+
+      <!-- goz2 el sign up -->
+
 <div id="signupbox">
 <form action="login.php" id="signupform" method="post">
 <label for="Firstname">First Name</label>
 <input type="text" name="Fname" id="Fname" class="txtfield"  autocomplete="off">
     <label for="Lastname">Last Name</label>
 <input type="text" name="Lname" id="Lname" class="txtfield"  autocomplete="off">
-   
+
     <p>Enter you birthdate:</p>
 <input type="date" name="bday" min="1950-01-02" max="2000-12-31"><br>
     <label for="phone">Phone</label>
 <input type="phone" name="phone" id="phone" autocomplete="off"> <br>
-  
+
 <label for="newemail">Email Address</label>
 <input type="email" name="newemail" id="newemail" class="txtfield"  autocomplete="off">
 <label for="password1">Password</label>
 <input type="password" name="password1" id="password1" class="txtfield"  autocomplete="off">
 <label for="password2">Confirm Password</label>
 <input type="password" name="password2" id="password2" class="txtfield" autocomplete="off"><br>
-   
+
      <p> Choose your gender</p>
     <input type="radio" name="gender" value="male" class="radio" checked> Male<br>
   <input type="radio" name="gender" value="female" class="radio"> Female<br>
-    
+
     <p id="type">Your type of insurance?</p>
     <input type="radio" name="individual" value="Individual" class="radio" checked> Individual<br>
   <input type="radio" name="insuredbycomapny" value="Insured by comapny" class="radio"> Insured by comapny<br>
-    
+
     <p id="name">*Write the name of insurance company:</p>
     <label for="name"></label>
     <input type="name" id="name" class="txtfield">
-    
-   
+
+
     <input type="submit" name="registerbtn"  id="registerbtn" value="SIGN Up" class="btn">
-    
+
 
 
 </form>
@@ -182,7 +182,7 @@ else {
                     <img src="fb.png">
                     <img src="Linkedin.png">
                     <img src="twitter.png">
-                 
+
                 </div>
                 </footer>
 
--- profile.php
+++ profile.php
@@ -1,13 +1,17 @@
 <?php
-
 session_start();
 
+require 'dbconfig/config.php';
+
+$query = "select * from patient";// WHERE E_MAIL= '" . $username= $_SESSION['username'] . "'";
+$query_run = mysqli_query($con,$query);
+$row = mysqli_fetch_assoc($query_run);
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    
+
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,88 +20,94 @@ session_start();
         <link rel="stylesheet" href="try.css">
         <body id="body_profile">
         <div id="divwrapper">
-            <div id="divheader"> 
-		   
+            <div id="divheader">
+
 		   <h1> Insurancy</h1></div>
            <div id="divLine"></div>
         <div id="divLinks">
-           <div id="divNav"> 
-	
-       
-    
+           <div id="divNav">
+
+
+
                 <ul>
                     <li><a href="Home.html">Home</a></li>
                     <li><a href="login.html"class="current">Profile</a></li>
                     <li><a href="services.html">Services</a></li>
                     <li><a href="contact us.html">Contact us</a></li>
-                    
-                    
+
+
 
 
 
                 </ul>
 
             </div>
-           
-           
-           
+
+
+
            </div>
-        
+
         </div>
            <div class="profile-info">
 	<br>
             <marquee> Put your Ads here</marquee>
-			
-      <div class="patient"id="p_info">  
-        
-          
+
+      <div class="patient"id="p_info">
+
+
         <img  src="chemical.jpg"style="width:180px;height:180px;margin-left:25px;margin-top:20px;">
 		  <div class="b.photo">
      </div>
 
 	 <div class="div1">
 	 </div>
-	 
+
 	 <br>
 	<ul class="b">
   <li>NAME:
-<?php echo $_SESSION['E_MAIL']; ?>
+<?php echo $row['FIRST_NAME']; ?>
   </li>
   <br>
-      <li>Address:</li>
+      <li>Address: </li>
    <br>
-  <li>AGE:</li>
+  <li>AGE:
+
+  </li>
   <br>
-   <li>GENDER:</li>
-  
+   <li>GENDER:
+<?php echo $row['GENDER']; ?>
+   </li>
+
   <br>
-  <li>Phone:</li>
+  <li>Phone:
+<?php echo $row['PHONE']; ?>
+  </li>
   <br>
 
 <li>INSURED BY:</li>
    <br>
-      
+
   <li> COUPONS:</li>
- 
-  
+
+
 </ul>
 	 </div>
 		<!--   <div class="p_history">  <!--الاماكن اللي زرها المريض -->
-        <!-- 
-		
+        <!--
+
 		  </div> -->
     <div class="patient" id="p_history">  <!--معلومات عن المريض -->
-        
+
 
 <div class ="his">
-           <h2>HISTORY</h2> 
+           <h2>HISTORY</h2>
 		  </div>
 		   <div class="div2">
 	 </div>
 		  </div>
-      
+
    <div class="patient"id="p_recommendation">  <!--معلومات عن المريض -->
-        
+
           <div class="rec">
            <h3>RECOMMENDATIONS</h3>
 		  </div>
@@ -107,22 +117,21 @@ session_start();
    </div>
 	       </div>
          </div>
-		  
+
  <footer id="footer_profile">
-  
-   
-               <div> 
+
+
+               <div>
                     <img src="fb.png">
                     <img src="Linkedin.png">
                     <img src="twitter.png">
-                 
-                
-             
+
+
+
 <p>Copy right 2017 &copy; All rights reserved to .. </p></footer>
             <!-- End footer -->
-            </footer>  
-       
- </body>
-	  
-</html>
+            </footer>
 
+ </body>
+
+</html>
